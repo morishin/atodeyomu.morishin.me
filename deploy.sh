4e34e932f3f596eca4e565673386630b9fe0a5d7
morishin <sntr92@gmail.com>
2024-09-12 15:25:58 +0900

    Use curl

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ set -eux
 
 npm run build
 
-wget -O - https://github.com/sqldef/sqldef/releases/latest/download/psqldef_linux_amd64.tar.gz | tar xvz
+curl -LO https://github.com/sqldef/sqldef/releases/latest/download/psqldef_linux_amd64.tar.gz
+tar -xzf psqldef_linux_amd64.tar.gz
 chmod +x psqldef
 
 ./psqldef -h $POSTGRES_HOST -U $POSTGRES_USER -W $POSTGRES_PASSWORD $POSTGRES_DATABASE --enable-drop-table < db/schema.sql
