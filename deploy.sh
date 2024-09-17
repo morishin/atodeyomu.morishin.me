1b77cf16153ecbb767e6852c8615971a6468879b
morishin <sntr92@gmail.com>
2024-09-17 14:45:09 +0900

    Skip migration

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ set -eux
 
 npm run build
 
-curl -LO https://github.com/sqldef/sqldef/releases/latest/download/psqldef_linux_amd64.tar.gz
-tar -xzf psqldef_linux_amd64.tar.gz
-chmod +x psqldef
-
-./psqldef -h $POSTGRES_HOST -U $POSTGRES_USER -W $POSTGRES_PASSWORD $POSTGRES_DATABASE --enable-drop-table < db/schema.sql
+# Pending migration: https://github.com/sqldef/sqldef/issues/451
+# curl -LO https://github.com/sqldef/sqldef/releases/latest/download/psqldef_linux_amd64.tar.gz
+# tar -xzf psqldef_linux_amd64.tar.gz
+# chmod +x psqldef
+#
+# ./psqldef -h $POSTGRES_HOST -U $POSTGRES_USER -W $POSTGRES_PASSWORD $POSTGRES_DATABASE --enable-drop-table < db/schema.sql
